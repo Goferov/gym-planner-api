820983de904e9d0604e6293ca9acd1cb07302c6c
Goferov <marcingodfryd@gmail.com>
2025-03-04 20:40:46 +0100

    Create migrations

---
+++ database/migrations/2025_03_04_184700_create_plan_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plan_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('plan_id')
+                ->constrained('plans');
+            $table->foreignId('user_id')
+                ->constrained('users');
+            $table->timestamp('assigned_at')->useCurrent();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plan_user');
+    }
+};
